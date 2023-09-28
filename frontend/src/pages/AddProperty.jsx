d86b34440bb4a099d275457c421894b811068255
Cedricsia <cedricsia@hotmail.fr>
2023-09-28 15:51:37 +0200

    ameli

--- frontend/src/pages/AddProperty.jsx
+++ frontend/src/pages/AddProperty.jsx
@@ -19,7 +19,6 @@ function AddProperty() {
     },
     equipments: [],
   });
-
   useEffect(() => {
     ExpressApi.get("/equipment")
       .then((res) => setEquip(res.data))
@@ -67,12 +66,15 @@ function AddProperty() {
   };
 
   return (
-    <div className=" flex flex-col">
-      <h1>Mettre votre bien en ligne</h1>
+    <div className=" flex flex-col mx-2 md:max-w-3xl lg:max-w-7xl md:mx-auto">
+      <h1 className="text-center text-primary mt-10 font-bold text-xl">
+        Mettre votre bien en ligne
+      </h1>
       <div>
-        <h1>Infos generale :</h1>
-        <form action="">
-          <div>
+        <h1 className="mt-5 mb-3 text-center font-bold">Infos generale :</h1>
+
+        <div className="flex flex-col mx-2 gap-3 my-3 ">
+          <div className="flex-row justify-between flex">
             <label htmlFor="name">nom de l'annonce :</label>
             <input
               type="text"
@@ -80,30 +82,35 @@ function AddProperty() {
               onChange={(e) => handleInputChange(e, "name")}
             />
           </div>
-          <div>
-            <label htmlFor="information">Resumé de l'annonce </label>
-            <input
-              type="text"
-              className="bg-slate-200"
-              onChange={(e) => handleInputChange(e, "information")}
-            />
-          </div>
-          <div>
-            <label htmlFor="localisation">Ville de votre bien </label>
+          <div className="flex-row justify-between flex">
+            <label htmlFor="localisation">Ville de votre bien:</label>
             <input
               type="text"
               className="bg-slate-200"
               onChange={(e) => handleInputChange(e, "localisation")}
             />
           </div>
-          <div>
-            <h1>Equipements</h1>
+          <div className="flex-col flex">
+            <label htmlFor="information">Resumé de l'annonce :</label>
+            <textarea
+              maxLength={170}
+              type="text"
+              className="bg-slate-200 h-28"
+              onChange={(e) => handleInputChange(e, "information")}
+            />
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <h1 className="text-center font-bold">Equipements :</h1>
+
+          <div className=" grid  grid-cols-2 md:grid-cols-4 gap-1 mx-3 my-3 ">
             {equip &&
               equip.map((elem) => (
-                <li className="flex flex-row">
+                <li className="flex flex-row justify-between items-center border border-primary mx-1 p-2">
                   <p>{elem.name}</p>
-
                   <input
+                    className="mx-2"
                     type="checkbox"
                     name={elem.name}
                     id=""
@@ -112,35 +119,39 @@ function AddProperty() {
                 </li>
               ))}
           </div>
-        </form>
-        <div>
+        </div>
+
+        <div className="mx-2">
+          <h1 className="text-center font-bold mt-10 my-3">Photos:</h1>
+          <p>Sélectionné vos photos :</p>
           <input
+            className="p-3"
             type="file"
             name=""
             id=""
             multiple
-            onChange={(e) =>
-              setFiles([...files, ...Array.from(e.target.files)])
-            }
+            onChange={(e) => setFiles([...files, ...e.target.files])}
           />
-          <div>
+          <div className="flex flex-col mx-3 gap-1 md:grid md:grid-cols-3">
             {files &&
               files.map((elem, index) => (
                 <img
                   src={URL.createObjectURL(files[index])}
                   alt=""
-                  className="h-10"
+                  className=" lg:h-56"
                 />
               ))}
           </div>
         </div>
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={() => submit()}
-        >
-          go
-        </button>
+        <div className="flex  justify-center my-10">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => submit()}
+          >
+            Envoyez votre bien
+          </button>
+        </div>
       </div>
     </div>
   );
